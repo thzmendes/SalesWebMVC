0cd9968dc7f94c6f5c57246a44acc6be72eaa61c
tmendes <th.mendes1997@gmail.com>
2019-06-05 21:13:26 -0300

    First Model-Controller-View / Department

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public Department(int iD, string name)
+        {
+            ID = iD;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+    }
+}
