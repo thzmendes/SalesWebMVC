a4b72547e4e062db7cd107874ba7b7ec94faa622
tmendes <th.mendes1997@gmail.com>
2019-06-17 19:45:29 -0300

    Sellers Service, Find All

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using SalesWebMVC.Services;
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService sellerService;
+
+        public SellersController (SellerService sellerService)
+        {
+            this.sellerService= sellerService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var list = sellerService.FindAll();
+            return View(list);
         }
     }
 }
---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext context;
+
+        public SellerService(SalesWebMVCContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return context.Seller.ToList();
+        }
+
+    }
+}
